b81eabf1e8ae31b9c148815c07f3e6f452836641
AgathePons <ponsagathe37@gmail.com>
2022-11-23 09:58:40 +0100

    add: TU trainee dto

--- src/test/java/survey/backend/dto/TraineeDtoTest.java
+++ src/test/java/survey/backend/dto/TraineeDtoTest.java
@@ -2,6 +2,8 @@ package survey.backend.dto;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TraineeDtoTest {
@@ -18,6 +20,42 @@ class TraineeDtoTest {
     );
   }
 
-  // TODO all args
-  // TODO builder
+  @Test
+  void testAllArgsConstructor() {
+    int id = 666;
+    String lastname = "Durand";
+    String firstname = "Jean";
+    String email = "jeanmi@mail.com";
+    String phoneNumber = "0625487556";
+    var birthdate = LocalDate.of(1975, 7, 12);
+    var traineeDto = new TraineeDto(id, lastname, firstname, email, phoneNumber, birthdate);
+    assertAll(
+            () -> assertEquals(id, traineeDto.getId(), "id"),
+            () -> assertEquals(lastname, traineeDto.getLastName(), "lastname"),
+            () -> assertEquals(firstname, traineeDto.getFirstName(), "firstname"),
+            () -> assertEquals(email, traineeDto.getEmail(), "email"),
+            () -> assertEquals(phoneNumber, traineeDto.getPhoneNumber(), "phoneNumber"),
+            () -> assertEquals(birthdate, traineeDto.getBirthDate(), "birthdate")
+    );
+  }
+
+  @Test
+  void testBuilder() {
+    String firstname = "Jean";
+    String email = "jeanmi@mail.com";
+    var birthdate = LocalDate.of(1975, 7, 5);
+    var traineeDto = TraineeDto.builder()
+            .firstName(firstname)
+            .email(email)
+            .birthDate(birthdate)
+            .build();
+    assertAll(
+            () -> assertNull(traineeDto.getId(), "id"),
+            () -> assertNull(traineeDto.getLastName(), "lastName"),
+            () -> assertEquals(firstname, traineeDto.getFirstName(), "firstname"),
+            () -> assertEquals(email, traineeDto.getEmail(), "email"),
+            () -> assertNull(traineeDto.getPhoneNumber(), "phoneNumber"),
+            () -> assertEquals(birthdate, traineeDto.getBirthDate(), "birthdate")
+    );
+  }
 }
