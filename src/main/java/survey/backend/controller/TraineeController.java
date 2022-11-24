c4d58d326d2de6b3cbe07541f1c1e096ebe4e124
AgathePons <ponsagathe37@gmail.com>
2022-11-24 12:02:39 +0100

    update: trainee controller + trainee service to move fake from controller to service

--- src/main/java/survey/backend/controller/TraineeController.java
+++ src/main/java/survey/backend/controller/TraineeController.java
@@ -8,7 +8,6 @@ import survey.backend.error.NoDataFoundError;
 import survey.backend.service.TraineeService;
 
 import java.time.LocalDate;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
@@ -25,40 +24,8 @@ public class TraineeController {
    * @return
    */
   @GetMapping
-  public Set<TraineeDto> list() {
+  public Set<TraineeDto> getAll() {
     return  traineeService.findAll();
-//    var trainee1 = TraineeDto.builder()
-//            .id(1)
-//            .firstName("John")
-//            .lastName("Doe")
-//            .birthDate(LocalDate.of(1945, 5, 3))
-//            .build();
-//    var trainee2 = TraineeDto.builder()
-//            .id(2)
-//            .firstName("Jane")
-//            .lastName("Doe")
-//            .birthDate(LocalDate.of(1900, 12, 24))
-//            .build();
-//    var trainee3 = TraineeDto.builder()
-//            .id(3)
-//            .firstName("Micheline")
-//            .lastName("Duduche")
-//            .birthDate(LocalDate.of(2001, 11, 18))
-//            .build();
-//    var trainee4 = TraineeDto.builder()
-//            .id(4)
-//            .firstName("Jean-Michèle")
-//            .lastName("Apeuprai")
-//            .birthDate(LocalDate.of(1990, 6, 30))
-//            .build();
-//    var trainee5 = TraineeDto.builder()
-//            .id(4)
-//            .firstName("Philomène")
-//            .lastName("Dupuy")
-//            .birthDate(LocalDate.of(1650, 9, 1))
-//            .build();
-//    var traineeSet = Set.of(trainee1, trainee2, trainee3, trainee4, trainee5);
-//    return traineeSet;
   }
 
   /**
@@ -83,30 +50,31 @@ public class TraineeController {
    * route /api/trainee/search?fn=John&ln=Doe
    * @param firstName (optional)
    * @param lastName (optional)
-   * @return trainee found
+   * @return trainee corresponding
    */
   @GetMapping("search")
   public Set<TraineeDto> search(
           @RequestParam(name="fn", required = false) String lastName,
           @RequestParam(name="ln", required = false) String firstName
   ) {
-    var trainee1 = TraineeDto.builder()
-            .id(1)
-            .firstName(Objects.isNull(firstName) ? "Found" : firstName)
-            .lastName(Objects.isNull(lastName) ? "Funny" : lastName)
-            .build();
-    var trainee2 = TraineeDto.builder()
-            .id(2)
-            .firstName("Jim")
-            .lastName("Found")
-            .build();
-    var trainee3 = TraineeDto.builder()
-            .id(3)
-            .firstName("James")
-            .lastName("Found")
-            .build();
-    var traineeSet = Set.of(trainee1, trainee2, trainee3);
-    return traineeSet;
+    return traineeService.search(lastName, firstName);
+//    var trainee1 = TraineeDto.builder()
+//            .id(1)
+//            .firstName(Objects.isNull(firstName) ? "Found" : firstName)
+//            .lastName(Objects.isNull(lastName) ? "Funny" : lastName)
+//            .build();
+//    var trainee2 = TraineeDto.builder()
+//            .id(2)
+//            .firstName("Jim")
+//            .lastName("Found")
+//            .build();
+//    var trainee3 = TraineeDto.builder()
+//            .id(3)
+//            .firstName("James")
+//            .lastName("Found")
+//            .build();
+//    var traineeSet = Set.of(trainee1, trainee2, trainee3);
+//    return traineeSet;
   }
 
   @PostMapping
@@ -117,26 +85,22 @@ public class TraineeController {
 
   @DeleteMapping("{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void removeById(@PathVariable("id") int id) {
-    // TODO remove
-
+  public boolean removeById(@PathVariable("id") int id) {
+    boolean isExisting = traineeService.delete(id);
+    if(isExisting) {
+      return true;
+    } else {
+      throw NoDataFoundError.withId("Trainee", id);
+    }
   }
 
   @PutMapping("/{id}")
   public TraineeDto updatePartialById(@PathVariable("id") int id, @RequestBody TraineeDto updatedTrainee) {
-    var trainee1 = TraineeDto.builder()
-            .id(666)
-            .firstName("John")
-            .lastName("Doe")
-            .email("johndoe@mail.com")
-            .phoneNumber("06 55 88 99 66")
-            .birthDate(LocalDate.of(1900, 7, 1))
-            .build();
-    trainee1.setFirstName(updatedTrainee.getFirstName());
-    trainee1.setLastName(updatedTrainee.getLastName());
-    trainee1.setEmail(updatedTrainee.getEmail());
-    trainee1.setPhoneNumber(updatedTrainee.getPhoneNumber());
-    trainee1.setBirthDate(updatedTrainee.getBirthDate());
-    return  trainee1;
+    Optional<TraineeDto> optTraineeDto = traineeService.update(updatedTrainee);
+    if(optTraineeDto.isPresent()) {
+      return updatedTrainee;
+    } else {
+      throw NoDataFoundError.withId("Trainee", id);
+    }
   }
 }
