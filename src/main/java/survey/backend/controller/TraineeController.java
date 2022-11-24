39d9bfbb6c4c9f17afade624830e662a49ac4db8
AgathePons <ponsagathe37@gmail.com>
2022-11-24 15:17:11 +0100

    update: fix sonar lint issues

--- src/main/java/survey/backend/controller/TraineeController.java
+++ src/main/java/survey/backend/controller/TraineeController.java
@@ -7,14 +7,13 @@ import survey.backend.dto.TraineeDto;
 import survey.backend.error.NoDataFoundError;
 import survey.backend.service.TraineeService;
 
-import java.time.LocalDate;
 import java.util.Optional;
 import java.util.Set;
 
 @RestController
 @RequestMapping("api/trainee")
 public class TraineeController {
-
+  final String ITEM_TYPE = "Trainee";
   @Autowired // DI (Dependency Injection)
   private TraineeService traineeService;
 
@@ -40,7 +39,7 @@ public class TraineeController {
     if (optTraineeDto.isPresent()) {
       return optTraineeDto.get();
     } else {
-      throw NoDataFoundError.withId("Trainee", id);
+      throw NoDataFoundError.withId(ITEM_TYPE, id);
 
     }
   }
@@ -58,23 +57,6 @@ public class TraineeController {
           @RequestParam(name="ln", required = false) String firstName
   ) {
     return traineeService.search(lastName, firstName);
-//    var trainee1 = TraineeDto.builder()
-//            .id(1)
-//            .firstName(Objects.isNull(firstName) ? "Found" : firstName)
-//            .lastName(Objects.isNull(lastName) ? "Funny" : lastName)
-//            .build();
-//    var trainee2 = TraineeDto.builder()
-//            .id(2)
-//            .firstName("Jim")
-//            .lastName("Found")
-//            .build();
-//    var trainee3 = TraineeDto.builder()
-//            .id(3)
-//            .firstName("James")
-//            .lastName("Found")
-//            .build();
-//    var traineeSet = Set.of(trainee1, trainee2, trainee3);
-//    return traineeSet;
   }
 
   @PostMapping
@@ -90,7 +72,7 @@ public class TraineeController {
     if(isExisting) {
       return true;
     } else {
-      throw NoDataFoundError.withId("Trainee", id);
+      throw NoDataFoundError.withId(ITEM_TYPE, id);
     }
   }
 
@@ -100,7 +82,7 @@ public class TraineeController {
     if(optTraineeDto.isPresent()) {
       return updatedTrainee;
     } else {
-      throw NoDataFoundError.withId("Trainee", id);
+      throw NoDataFoundError.withId(ITEM_TYPE, id);
     }
   }
 }
--- src/main/java/survey/backend/service/impl/DummyTraineeService.java
+++ src/main/java/survey/backend/service/impl/DummyTraineeService.java
@@ -65,20 +65,21 @@ public class DummyTraineeService implements TraineeService {
 
   @Override
   public Set<TraineeDto> search(String lastName, String firstName) {
+    final String DEFAULT_FAMILY = "Found";
         var trainee1 = TraineeDto.builder()
             .id(1)
-            .firstName(Objects.isNull(firstName) ? "Found" : firstName)
+            .firstName(Objects.isNull(firstName) ? DEFAULT_FAMILY : firstName)
             .lastName(Objects.isNull(lastName) ? "Funny" : lastName)
             .build();
     var trainee2 = TraineeDto.builder()
             .id(2)
             .firstName("Jim")
-            .lastName("Found")
+            .lastName(DEFAULT_FAMILY)
             .build();
     var trainee3 = TraineeDto.builder()
             .id(3)
             .firstName("James")
-            .lastName("Found")
+            .lastName(DEFAULT_FAMILY)
             .build();
     var traineeSet = Set.of(trainee1, trainee2, trainee3);
     return traineeSet;
