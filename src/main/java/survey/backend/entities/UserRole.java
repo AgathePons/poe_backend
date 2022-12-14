16ce19a3e543757e26f5fd6b5a309e0bc2d0404c
AgathePons <ponsagathe37@gmail.com>
2022-12-14 10:24:51 +0100

    add: entity user + user role

---
+++ src/main/java/survey/backend/entities/UserRole.java
@@ -0,0 +1,23 @@
+package survey.backend.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="user_role")
+@Getter @Setter
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "role", nullable = false)
+    private String role;
+
+    @JoinColumn(name = "user_id")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private User user;
+}
