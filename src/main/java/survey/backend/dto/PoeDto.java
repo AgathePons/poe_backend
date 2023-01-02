042a0aaca170fc0703e215b85d1933c154a3b182
AgathePons <ponsagathe37@gmail.com>
2023-01-02 16:55:18 +0100

    add date json format annotation

--- src/main/java/survey/backend/dto/PoeDto.java
+++ src/main/java/survey/backend/dto/PoeDto.java
@@ -1,5 +1,6 @@
 package survey.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import survey.backend.entities.Poe;
@@ -16,7 +17,9 @@ public class PoeDto {
   private Long id;
   private String title;
 
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
   private Date beginDate;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
   private Date endDate;
   private PoeType type;
 
