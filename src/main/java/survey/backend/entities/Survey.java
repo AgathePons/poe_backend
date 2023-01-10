99627fed2365dc14d214016b9dd0936484502993
AgathePons <ponsagathe37@gmail.com>
2023-01-10 15:10:39 +0100

    add annotations to survey entity

--- src/main/java/survey/backend/entities/Survey.java
+++ src/main/java/survey/backend/entities/Survey.java
@@ -3,10 +3,22 @@ package survey.backend.entities;
 import survey.backend.enums.Level;
 import survey.backend.enums.PoeType;
 
+import javax.persistence.*;
+
+@Entity
 public class Survey {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false, length = 10)
+    @Enumerated(EnumType.STRING)
     private Level level;
+
+    @Column(length = 4)
+    @Enumerated(EnumType.STRING)
     private PoeType poeType;
-    private String answerType;
 }
