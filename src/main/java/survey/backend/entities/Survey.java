709ddda86ff907aa1ce6a2f51787530b9afed2ef
AgathePons <ponsagathe37@gmail.com>
2023-01-10 16:29:00 +0100

    add questions in survey and many to many association

--- src/main/java/survey/backend/entities/Survey.java
+++ src/main/java/survey/backend/entities/Survey.java
@@ -6,6 +6,8 @@ import survey.backend.enums.Level;
 import survey.backend.enums.PoeType;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter @Setter
@@ -24,4 +26,13 @@ public class Survey {
     @Column(length = 4)
     @Enumerated(EnumType.STRING)
     private PoeType poeType;
+
+    @ManyToMany
+    @JoinTable(
+            name = "survey_contains_question",
+            joinColumns = @JoinColumn(name = "survey_id"),
+            inverseJoinColumns = @JoinColumn(name = "question_id")
+    )
+    //@Transient // to cancel persistance, while dev
+    private Set<Question> questions = new HashSet<>();
 }
