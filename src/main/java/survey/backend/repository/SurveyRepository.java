f08dd11bc303f4ca4eb0ef0d5b80146c638c9796
Fanny <tabaglio.fanny@gmail.com>
2023-01-11 11:42:06 +0100

    Add AnswerRepository & QuestionRepository

--- src/main/java/survey/backend/repository/SurveyRepository.java
+++ src/main/java/survey/backend/repository/SurveyRepository.java
@@ -3,5 +3,5 @@ package survey.backend.repository;
 import org.springframework.data.repository.CrudRepository;
 import survey.backend.entities.Survey;
 
-public interface SurveyRepository extends CrudRepository<Survey, Integer> {
+public interface SurveyRepository extends CrudRepository<Survey, Long> {
 }
