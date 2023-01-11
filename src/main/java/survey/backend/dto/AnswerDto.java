05243547973e8e98832195990ed02b086f6059cc
Fanny <tabaglio.fanny@gmail.com>
2023-01-11 11:27:16 +0100

    Add AnswerDto

---
+++ src/main/java/survey/backend/dto/AnswerDto.java
@@ -0,0 +1,13 @@
+package survey.backend.dto;
+
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AnswerDto {
+    private Long id;
+    private String text;
+}
