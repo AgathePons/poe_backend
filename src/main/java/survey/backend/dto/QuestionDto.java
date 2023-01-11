c8f2246aed0f3ff35ac8c4ae4380a80b6f86f9c8
Fanny <tabaglio.fanny@gmail.com>
2023-01-11 11:12:04 +0100

    add QuestionDtoi

---
+++ src/main/java/survey/backend/dto/QuestionDto.java
@@ -0,0 +1,15 @@
+package survey.backend.dto;
+
+import lombok.*;
+import survey.backend.enums.AnswerType;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class QuestionDto {
+    private Long id;
+    private String text;
+    private AnswerType answerType;
+}
